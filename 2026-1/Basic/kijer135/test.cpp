0e4ef8d8473f160a985b9c5695da6f8ef26a06c9
kijer135 <kijer135@gmail.com>
2026-03-26 18:28:39 +0900

    dkfas

--- 2026-1/Basic/kijer135/test.cpp
+++ 2026-1/Basic/kijer135/test.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main() 
+
+int main()
 {
-    for(int i = 0; i < 5; i++) {
-        cout << "Hello, World!" << endl;
-    }
-    return 0;
+	int num = 0;
+
+	cin >> num;
+
+	for (size_t i = 1; i <= num; i++)
+	{
+		for (size_t k = 0; k < i - 1; k++) cout << " ";
+		for (size_t j = 0; j < 2 * num - (i * 2 - 1); j++) cout << "*";
+		cout << "\n";
+	}
+
+	for (size_t i = 2; i <= num-1; i++)
+	{
+		for (size_t k = 0; k < num - i; k++) cout << " ";
+		for (size_t j = 0; j < 2 * i - 1; j++) cout << "*";
+		cout << "\n";
+	}
+    
+	if (num > 1) for (size_t i = 0; i < 2 * num - 1; i++) cout << "*";
 }
