22bad649fc1917f18e671229b63c9443a1a4bbc3
kush0u0v <kush0u0v@gmail.com>
2026-04-02 16:59:01 +0900

    3_4

--- 2026-1/Basic/kush0u0v/기초 코드 작성 요령 II/2753.cpp
+++ 2026-1/Basic/kush0u0v/기초 코드 작성 요령 II/2753.cpp
@@ -3,5 +3,6 @@
 int main(){
     int yoon;
     scanf("%d", &yoon);
-    printf("%d", (yoon % 4 == 0 && yoon % 100 != 0) || (yoon % 400 == 0) ? 1 : 0);
+    printf("%d", (yoon % 4 == 0 && yoon % 100 != 0) 
+            || (yoon % 400 == 0) ? 1 : 0);
 }
